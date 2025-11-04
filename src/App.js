c9dc8f1a4750c8b9beb0bafce4e0e65956abbebb
Janmesh Raut <janmeshraut0607@gmail.com>
2025-11-04 13:07:56 +0530

    First Commite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./components/Login";
+import DoctorDashboard from "./components/DoctorDashboard";
+import PatientDashboard from "./components/PatientDashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (role, name) => {
+    setUser({ role, name });
+  };
+
+  if (!user) return <Login onLogin={handleLogin} />;
+
+  if (user.role === "doctor") return <DoctorDashboard user={user} />;
+  if (user.role === "patient") return <PatientDashboard user={user} />;
 }
 
 export default App;
